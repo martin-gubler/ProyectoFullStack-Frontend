8b462d102b59def31defe1355c3114aea564cae2
martin.gubler <martingubler04@gmail.com>
2024-12-10 10:38:12 -0300

    cambio url back copiada de vercel

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,5 +1,5 @@
 const ENVIROMENT = {
-    'URL_BACKEND': 'mongodb+srv://test:laWoWyaDFYMtsEle@cluster0.who5m.mongodb.net'
+    'URL_BACKEND': 'https://proyecto-full-stack-backend-kvplpzknj-martin-gublers-projects.vercel.app'
 }
 
 export default ENVIROMENT
