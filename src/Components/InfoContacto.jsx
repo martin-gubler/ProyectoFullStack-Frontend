67661b00c2a73389dda957dd31e096d6858c3592
martin.gubler <martingubler04@gmail.com>
2024-12-10 19:10:31 -0300

    message

--- src/Components/InfoContacto.jsx
+++ src/Components/InfoContacto.jsx
@@ -16,7 +16,7 @@ const InfoContacto = () => {
           const response = await fetch(`${ENVIROMENT.URL_BACKEND}/api/contacts/InfoContacto/${receiver_id}`, {
               headers: getAuthenticatedHeaders()
           });
-          console.log(response.body)
+          console.log(response.message)
           if(!response.ok){
             throw new Error(`Error en la solicitud: ${response.statusText}`)
           }
