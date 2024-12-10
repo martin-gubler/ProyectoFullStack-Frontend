d258f69c45ce996b5931dcc397ee3e17efccfca9
martin.gubler <martingubler04@gmail.com>
2024-12-10 10:32:53 -0300

    cambio url back sacamos bara

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,5 +1,5 @@
 const ENVIROMENT = {
-    'URL_BACKEND': 'mongodb+srv://test:laWoWyaDFYMtsEle@cluster0.who5m.mongodb.net/'
+    'URL_BACKEND': 'mongodb+srv://test:laWoWyaDFYMtsEle@cluster0.who5m.mongodb.net'
 }
 
 export default ENVIROMENT
