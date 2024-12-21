e132ec1cf4da0340bbb7bdab308131d44b349109
martin.gubler <martingubler04@gmail.com>
2024-12-20 23:27:43 -0300

    cambio url back

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,5 +1,6 @@
 const ENVIROMENT = {
-    'URL_BACKEND': 'https://proyecto-full-stack-backend-28fge1gle-martin-gublers-projects.vercel.app/'
+    'URL_BACKEND': 'https://proyecto-full-stack-backend-28fge1gle-martin-gublers-projects.vercel.app'
+    /* vieja: https://proyecto-full-stack-backend-kvplpzknj-martin-gublers-projects.vercel.app */
 }
 
 export default ENVIROMENT
